e11b4b2efd3cdf90c2822c0a8842178f451c752c
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-03 02:55:28 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,13 @@
 # Make your shoe class here!
 class Shoe
+    attr_reader :brand
 
     def initialize(brand)
         @brand = brand
     end
 
-    def
+    def cobble
+        puts "Your shoe is as good as new!"
+    end
+
+    
